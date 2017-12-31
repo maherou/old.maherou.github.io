46e2e363e567f7a344b112152c0db90d40537123
Mike Heroux <maherou@sandia.gov>
2017-12-31 17:31:12 -0600

    Fix location error

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
+    "Lugano, Switzerland", 
+    46.0050102, 
+    8.9520281
+  ], 
+  [
     "Washington DC, USA", 
     38.8949549, 
     -77.0366456
   ], 
   [
-    "Berkeley CA, USA", 
-    37.8708393, 
-    -122.2728639
+    "Atlanta GA, USA", 
+    33.7490987, 
+    -84.3901849
   ], 
   [
-    "London, UK", 
-    51.5073219, 
-    -0.1276474
-  ], 
-  [
-    "Los Angeles, CA", 
-    34.054935, 
-    -118.244476
+    "Frankfurt, Germany", 
+    50.1106444, 
+    8.6820917
   ]
 ];
