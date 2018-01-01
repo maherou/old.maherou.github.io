995026bae063210b965ccc62248b801615713d12
Mike Heroux <maherou@sandia.gov>
2018-01-01 12:02:17 -0600

    Update event map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,18 +5,58 @@ var addressPoints = [
     8.9520281
   ], 
   [
-    "Washington DC, USA", 
-    38.8949549, 
-    -77.0366456
-  ], 
-  [
     "Atlanta GA, USA", 
     33.7490987, 
     -84.3901849
   ], 
   [
+    "Oak Ridge TN, USA", 
+    36.0103562, 
+    -84.2696449
+  ], 
+  [
+    "Basel, Switzerland", 
+    47.5581077, 
+    7.5878261
+  ], 
+  [
+    "Waikoloa Village HI, USA", 
+    19.942369, 
+    -155.787506
+  ], 
+  [
+    "Washington DC, USA", 
+    38.8949549, 
+    -77.0366456
+  ], 
+  [
+    "Knoxville TN, USA", 
+    35.9603948, 
+    -83.9210261
+  ], 
+  [
+    "Dallas TX, USA", 
+    32.7762719, 
+    -96.7968559
+  ], 
+  [
     "Frankfurt, Germany", 
     50.1106444, 
     8.6820917
+  ], 
+  [
+    "Tucson AZ, USA", 
+    32.2218917, 
+    -110.9262353
+  ], 
+  [
+    "Tokyo, Japan", 
+    34.2255804, 
+    139.294774527387
+  ], 
+  [
+    "Gaithersburg VA, USA", 
+    39.1434406, 
+    -77.2013705
   ]
 ];
